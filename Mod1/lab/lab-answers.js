dc75f0bdcc1d774548af149430d4ee624549dae2
tayl0rt0t <taylortread91@gmail.com>
2023-06-29 16:34:10 -0500

    wild wild life answered

--- Mod1/lab/lab-answers.js
+++ Mod1/lab/lab-answers.js
@@ -21,3 +21,23 @@ for(let i = 1; i <= 100; i++){
         console.log(i);
     }
 }
+
+//wild wild life
+const wolfy = ["Wolfy", "wolf", 16, "Yukon Territory"]
+const sharky = ["Sharky", "shark", 20, "Left Coast"]
+const plantee = ["Plantee", "plant",  5000 , "Mordor"]
+const porgee = ["Porgee", "Porg", 186, "Ahch-To"]
+const dart = ["D'Art" , "Demogorgan Dog", 2, "Upside Down"]
+
+//change plantee age
+plantee.splice(2,1,5001);
+console.log(plantee);
+//change wolfy hometown
+wolfy.splice(3,1,'Gotham City');
+console.log(wolfy);
+//dart add second hometown
+dart.push('Hawkins');
+console.log(dart);
+//wolfy change name
+wolfy.splice(0,1,"Gameboy");
+console.log(wolfy);
